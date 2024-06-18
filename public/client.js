e59d52472911f6adf61164f1111774afb597554c
pod1106 <i0503331711@gmail.com>
2024-06-18 14:18:55 +0300

    first commit

---
+++ public/client.js
@@ -0,0 +1,75 @@
+const socket = io();
+
+let roomName;
+let clickCount = 0;
+let opponentClickCount = 0;
+
+document.getElementById('startGameButton').addEventListener('click', () => {
+    document.getElementById('menu').style.display = 'none';
+    document.getElementById('game').style.display = 'block';
+    socket.emit('joinGame');
+});
+
+document.getElementById('backToMenuButton').addEventListener('click', () => {
+    console.log('back to menu')
+    document.getElementById('menu').style.display = 'block';
+    document.getElementById('game').style.display = 'none';
+    document.getElementById('gameover').style.display = 'none';
+    resetGame();
+});
+
+
+document.getElementById('backToMenuButton2').addEventListener('click', () => {
+    document.getElementById('menu').style.display = 'block';
+    document.getElementById('game').style.display = 'none';
+    document.getElementById('gameover').style.display = 'none';
+});
+
+socket.on('waitingForOpponent', () => {
+    document.getElementById('status').textContent = 'Waiting for an opponent...';
+});
+
+socket.on('startGame', (data) => {
+    roomName = data.roomName;
+    document.getElementById('status').textContent = 'Game started! First to 10 clicks wins!';
+    document.getElementById('clickButton').disabled = false;
+});
+
+document.getElementById('clickButton').addEventListener('click', () => {
+    clickCount++;
+    document.getElementById('clickCount').textContent = clickCount;
+    socket.emit('click', { roomName, clicks: clickCount });
+
+    if (clickCount >= 10) {
+        socket.emit('winGame', roomName);
+    }
+
+    
+});
+
+socket.on('opponentClick', (data) => {
+    opponentClickCount = data.opponentClicks;
+    document.getElementById('opponentClickCount').textContent = opponentClickCount;
+});
+
+socket.on('gameOver', (data) => {
+    document.getElementById('menu').style.display = 'none';
+    document.getElementById('game').style.display = 'none';
+    document.getElementById('gameover').style.display = 'block';
+    if (data.winner === socket.id) {
+        document.getElementById('winner').textContent = 'you won!';   
+    } else {
+        document.getElementById('winner').textContent = 'u lost.. maybe next time!';
+    }
+    
+
+});
+
+function resetGame() {
+    clickCount = 0;
+    opponentClickCount = 0;
+    document.getElementById('clickCount').textContent = clickCount;
+    document.getElementById('opponentClickCount').textContent = opponentClickCount;
+    document.getElementById('status').textContent = 'Waiting for an opponent...';
+    document.getElementById('clickButton').disabled = true;
+}
---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+const PORT = process.env.PORT || 3000;
+
+let waitingPlayer = null;
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+    console.log('A user connected:', socket.id);
+
+    socket.on('joinGame', () => {
+        if (waitingPlayer) {
+            const roomName = `room-${socket.id}-${waitingPlayer.id}`;
+            console.log(roomName);
+            socket.join(roomName);
+            waitingPlayer.join(roomName);
+
+            io.to(roomName).emit('startGame', { roomName });
+            waitingPlayer = null;
+        } else {
+            waitingPlayer = socket;
+            socket.emit('waitingForOpponent');
+        }
+    });
+
+    socket.on('click', (data) => {
+        const roomName = data.roomName;
+        const clickCount = data.clicks;
+        socket.to(roomName).emit('opponentClick', { opponentClicks: clickCount });
+    });
+
+
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected:', socket.id);
+        if (waitingPlayer === socket) {
+            waitingPlayer = null;
+        }
+    });
+
+    socket.on('winGame', (roomName) => {
+        console.log(`Player in ${roomName} won the game`);
+        io.to(roomName).emit('gameOver', { winner: socket.id });
+        io.in(roomName).socketsLeave(roomName);
+    });
+});
+
+server.listen(PORT, () => {
+    console.log(`link: http://localhost:${PORT}`);
+});
